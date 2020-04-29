2e680f9457abbc7af8d97194b36eab86acab5530
Anthony <anthony.tron@etu.univ-amu.fr>
2020-04-29 10:12:45 +0200

    Ex. 2 11/11 tests réussis

---
+++ iut-tp1/src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -0,0 +1,28 @@
+package fr.univ_amu.iut.exercice2;
+
+public class FizzBuzzer {
+
+    public String computeString(int i) {
+        String s;
+
+        if (i % 3 == 0 && i % 5 == 0)
+            s = "FizzBuzz";
+        else if (i % 3 == 0)
+            s = "Fizz";
+        else if (i % 5 == 0)
+            s = "Buzz";
+        else
+            s = Integer.toString(i);
+
+        return s;
+    }
+
+    public String[] computeList(int i) {
+        String[] arr = new String[i];
+
+        for (int j = 0; j < i; ++j)
+            arr[j] = computeString(j+1);
+
+        return arr;
+    }
+}
