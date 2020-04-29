d7f7cf5ccc85a61f8e2059e1be2b1a0ad1f96de2
Anthony <anthony.tron@etu.univ-amu.fr>
2020-04-29 09:47:40 +0200

    Ex. 1 4/4 tests réussis

---
+++ iut-tp1/src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -0,0 +1,10 @@
+package fr.univ_amu.iut.exercice1;
+
+public class HelloWorld {
+    public static String hello(String s) {
+        if (s == null || s.isEmpty())
+            return "Hello, World!";
+
+        return "Hello, " + s + "!";
+    }
+}
---
+++ iut-tp1/src/test/java/fr/univ_amu/iut/exercice1/HelloWorldTest.java
@@ -0,0 +1,30 @@
+package fr.univ_amu.iut.exercice1;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test_hello_with_empty_string_is_compared_by_value() {
+        assertEquals("Hello, World!", HelloWorld.hello(new String()));
+    }
+
+    @Test
+    public void test_hello_with_no_name_should_return_helloworld() {
+        assertEquals("Hello, World!", HelloWorld.hello(null));
+    }
+
+    @Test
+    public void test_hello_should_return_hello_alice_when_alice() {
+        assertEquals("Hello, Alice!", HelloWorld.hello("Alice"));
+    }
+
+
+    @Test
+    public void test_hello_should_return_hello_bob_when_bob() {
+        assertEquals("Hello, Bob!", HelloWorld.hello("Bob"));
+    }
+}
